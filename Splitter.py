c4cd0367ae4066c4d58438a8b0411f5851652c18
Oleksii Zaitsev <oleksii.animation@gmail.com>
2017-06-26 07:52:24 +0300

    Update Splitter.py

--- Splitter.py
+++ Splitter.py
@@ -6,7 +6,7 @@ import sys
 import threading
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
-
+import re
 
 app = QtGui.QApplication(sys.argv)
 window = QtGui.QWidget()
@@ -15,6 +15,28 @@ UI.setupUi(window)
 app.setWindowIcon(QtGui.QIcon('icon.ico'))
 
 UI.label_image.setPixmap(QPixmap(r"img\image.jpg"))
+
+
+
+
+def get_max_number_name(path) -> "return max number in name from dirs or files by path":
+    dirs_names = os.listdir(path)
+    list_of_numbers = []
+
+    for i in dirs_names:
+        number = re.findall('.*([-_][0-9]+).*', i)
+        if number:
+            list_of_numbers.append(*number)
+
+    try:
+        max_number = max([int(re.sub('[-_]', '', i)) for i in list_of_numbers])
+    except:
+        max_number = 0
+
+
+    return str(max_number + 1)
+
+
 
 def CubeW_Stereoscopic():
     UI.progressBar.setProperty("value", 0)
@@ -26,7 +48,7 @@ def CubeW_Stereoscopic():
     height = image.size[1]
     pix = image.load()
     w, h = image.size
-    dir_name = r'OpenGL_Stereoscopic_Cube_Map'
+    dir_name = r'OpenGL_Stereoscopic_Cube_Map -' + get_max_number_name(path)
 
     if not os.path.exists(os.path.join(path, dir_name, r"right")):
         os.makedirs(os.path.join(path, dir_name, r'right'))
@@ -63,7 +85,7 @@ def Spherical_Stereoscopic():
     height = image.size[1]
     pix = image.load()
     w, h = image.size
-    dir_name = r'Spherical_Stereoscopic'
+    dir_name = r'Spherical_Stereoscopic -' + get_max_number_name(path)
 
     if not os.path.exists(os.path.join(path, dir_name)):
         os.makedirs(os.path.join(path, dir_name))
@@ -83,7 +105,7 @@ def Unity_CubeW():
     height = image.size[1]
     pix = image.load()
     w, h = image.size
-    dir_name = r'Cube_Map_Unity'
+    dir_name = r'Cube_Map_Unity -' + get_max_number_name(path)
 
     if not os.path.exists(os.path.join(path, dir_name)):
         os.makedirs(os.path.join(path, dir_name))
@@ -107,7 +129,7 @@ def CubeW():
     height = image.size[1]
     pix = image.load()
     w, h = image.size
-    dir_name = r'Cube_Map'
+    dir_name = r'Cube_Map -' + get_max_number_name(path)
 
     if not os.path.exists(os.path.join(path, dir_name)):
         os.makedirs(os.path.join(path, dir_name))
@@ -132,7 +154,7 @@ def OpenGL_CubeW():
     height = image.size[1]
     pix = image.load()
     w, h = image.size
-    dir_name = r'OpenGL_CubeW'
+    dir_name = r'OpenGL_CubeW -' + get_max_number_name(path)
 
     if not os.path.exists(os.path.join(path, dir_name)):
         os.makedirs(os.path.join(path, dir_name))
@@ -156,7 +178,7 @@ def Unity_Cube_Stereoscopic():
     height = image.size[1]
     pix = image.load()
     w, h = image.size
-    dir_name = r'Unity_Cube_Stereoscopic'
+    dir_name = r'Unity_Cube_Stereoscopic -' + get_max_number_name(path)
 
     if not os.path.exists(os.path.join(path, dir_name, r"right")):
         os.makedirs(os.path.join(path, dir_name, r'right'))
@@ -183,6 +205,116 @@ def Unity_Cube_Stereoscopic():
     UI.progressBar.setProperty("value", 100)
     del image
 
+# def Oculus_Cubemap_Generator():
+#
+#     UI.progressBar.setProperty("value", 0)
+#     image = Image.open(UI.lineEdit_IMAGE_PATH.text())
+#     path = (UI.lineEdit_SAVE_PATH.text())
+#
+#     draw = ImageDraw.Draw(image)
+#     width = image.size[0]
+#     height = image.size[1]
+#     pix = image.load()
+#     w, h = image.size
+#     dir_name = r'Oculus_Cubemap_Generator -' + get_max_number_name(path)
+#
+#     if not os.path.exists(os.path.join(path, dir_name)):
+#         os.makedirs(os.path.join(path, dir_name))
+#
+#     image.crop((0,       0,      w/6,  h)).save(os.path.join(path, dir_name, r"Left_PX.png"), "PNG")
+#     image.crop((    w/6, 0,  (w/6)*2,  h)).save(os.path.join(path, dir_name, r"Right_NX.png"), "PNG")
+#     image.crop(((w/6)*2, 0,  (w/6)*3,  h)).save(os.path.join(path, dir_name, r"Up_PY.png"), "PNG")
+#
+#     UI.progressBar.setProperty("value", 50)
+#
+#     image.crop(((w/6)*3,     0,     (w/6)*4,  h)).save(os.path.join(path, dir_name, r"Down_NY.png"), "PNG")
+#     image.crop(((w/6)*4,     0,     (w/6)*5,  h)).save(os.path.join(path, dir_name, r"Back_PZ.png"), "PNG")
+#     image.crop(((w/6)*5,     0,     (w/6)*6,  h)).save(os.path.join(path, dir_name, r"Front_NZ.png"), "PNG")
+#
+#     # int(UI.lineEdit_X.text())
+#     # int(UI.lineEdit_Y.text())
+#
+#     const_X_size = int(int(UI.lineEdit_X.text())/6)
+#
+#
+#     images = [[os.path.join(path, dir_name, r"Left_PX.png"),  int(0),   int(0)]
+#             , [os.path.join(path, dir_name, r"Right_NX.png"), const_X_size,   int(0)]
+#             , [os.path.join(path, dir_name, r"Up_PY.png"),    const_X_size*2, int(0)]
+#             , [os.path.join(path, dir_name, r"Down_NY.png"),  const_X_size*3, int(0)]
+#             , [os.path.join(path, dir_name, r"Back_PZ.png"),  const_X_size*4, int(0)]
+#             , [os.path.join(path, dir_name, r"Front_NZ.png"), const_X_size*5, int(0)]]
+#
+#     UI.progressBar.setProperty("value", 75)
+#
+#     background = Image.new("RGB", (int(UI.lineEdit_X.text()), int(UI.lineEdit_Y.text())), (0, 0, 0, 0))
+#     for image, x, y in images:
+#         print("image :", image, "position x:", x, "position y:", y)
+#         image = Image.open(image).convert('RGB')
+#
+#         if h != int(UI.lineEdit_Y.text()) or w != int(UI.lineEdit_X.text()):
+#             background.paste(image.resize((int(UI.lineEdit_X.text()) // 6, int(UI.lineEdit_Y.text()))), (x, y))
+#         else:
+#          background.paste(image, (x, y))
+#
+#     background.save(os.path.join(path, dir_name, r"Oculus_Cubemap.png"), "PNG")
+#
+#     UI.progressBar.setProperty("value", 100)
+#     del image
+
+def Oculus_Cubemap_Generator_Left_Hand():
+
+    UI.progressBar.setProperty("value", 0)
+    image = Image.open(UI.lineEdit_IMAGE_PATH.text())
+    path = (UI.lineEdit_SAVE_PATH.text())
+
+    draw = ImageDraw.Draw(image)
+    width = image.size[0]
+    height = image.size[1]
+    pix = image.load()
+    w, h = image.size
+    dir_name = r'Oculus_Cubemap_Generator_Left_Hand -' + get_max_number_name(path)
+
+    if not os.path.exists(os.path.join(path, dir_name)):
+        os.makedirs(os.path.join(path, dir_name))
+
+    image.crop((0,       0,      w/6,  h)).transpose(Image.FLIP_LEFT_RIGHT).save(os.path.join(path, dir_name, r"Left_PX.png"), "PNG")
+    image.crop((    w/6, 0,  (w/6)*2,  h)).transpose(Image.FLIP_LEFT_RIGHT).save(os.path.join(path, dir_name, r"Right_NX.png"), "PNG")
+    image.crop(((w/6)*2, 0,  (w/6)*3,  h)).transpose(Image.FLIP_TOP_BOTTOM).save(os.path.join(path, dir_name, r"Up_PY.png"), "PNG")
+
+    UI.progressBar.setProperty("value", 50)
+
+    image.crop(((w/6)*3,     0,     (w/6)*4,  h)).transpose(Image.FLIP_TOP_BOTTOM).save(os.path.join(path, dir_name, r"Down_NY.png"), "PNG")
+    image.crop(((w/6)*4,     0,     (w/6)*5,  h)).transpose(Image.FLIP_LEFT_RIGHT).save(os.path.join(path, dir_name, r"Back_PZ.png"), "PNG")
+    image.crop(((w/6)*5,     0,     (w/6)*6,  h)).transpose(Image.FLIP_LEFT_RIGHT).save(os.path.join(path, dir_name, r"Front_NZ.png"), "PNG")
+
+
+    const_X_size = int(int(UI.lineEdit_X.text())/6)
+
+
+    images = [[os.path.join(path, dir_name, r"Left_PX.png"),  int(0),   int(0)]
+            , [os.path.join(path, dir_name, r"Right_NX.png"), const_X_size,   int(0)]
+            , [os.path.join(path, dir_name, r"Up_PY.png"),    const_X_size*2, int(0)]
+            , [os.path.join(path, dir_name, r"Down_NY.png"),  const_X_size*3, int(0)]
+            , [os.path.join(path, dir_name, r"Back_PZ.png"),  const_X_size*4, int(0)]
+            , [os.path.join(path, dir_name, r"Front_NZ.png"), const_X_size*5, int(0)]]
+
+    UI.progressBar.setProperty("value", 75)
+
+    background = Image.new("RGB", (int(UI.lineEdit_X.text()), int(UI.lineEdit_Y.text())), (0, 0, 0, 0))
+    for image, x, y in images:
+        print("image :", image, "position x:", x, "position y:", y)
+        image = Image.open(image).convert('RGB')
+
+        if h != int(UI.lineEdit_Y.text()) or w != int(UI.lineEdit_X.text()):
+            background.paste(image.resize((int(UI.lineEdit_X.text()) // 6, int(UI.lineEdit_Y.text()))), (x, y))
+        else:
+         background.paste(image, (x, y))
+
+    background.save(os.path.join(path, dir_name, r"Oculus_Cubemap.png"), "PNG")
+
+    UI.progressBar.setProperty("value", 100)
+    del image
+
 class programThreads():
     def __init__(self, func):
         self.func = func
@@ -199,7 +331,7 @@ UI.pushButton_Unity_CubeW.setIcon(QIcon(r"img\Unity_CubeW.png"))
 UI.pushButton_CubeW.setIcon(QIcon(r"img\CubeW.png"))
 UI.pushButton_OpenGL_CubeW.setIcon(QIcon(r"img\OpenGL_CubeW.png"))
 UI.pushButton_Unity_Cube_Stereoscopic.setIcon(QIcon(r"img\Unity_Cube_Stereoscopic.png"))
-
+UI.pushButton_Oculus_Cubemap_Generator.setIcon(QIcon(r"img\Oculus_Cubemap_Generator.png"))
 
 # THREADS
 Thread_CubeW_Stereoscopic = programThreads(CubeW_Stereoscopic)
@@ -208,6 +340,7 @@ Thread_CubeW = programThreads(CubeW)
 Thread_Unity_CubeW = programThreads(Unity_CubeW)
 Thread_OpenGL_CubeW = programThreads(OpenGL_CubeW)
 Thread_Unity_Cube_Stereoscopic = programThreads(Unity_Cube_Stereoscopic)
+Thread_Oculus_Cubemap_Generator = programThreads(Oculus_Cubemap_Generator_Left_Hand)
 
 # BUTTONS
 QtCore.QObject.connect(UI.pushButton_Spherical_Stereoscopic, QtCore.SIGNAL("clicked()"), lambda: Thread_Spherical_Stereoscopic.START())
@@ -216,7 +349,7 @@ QtCore.QObject.connect(UI.pushButton_Unity_CubeW, QtCore.SIGNAL("clicked()"), la
 QtCore.QObject.connect(UI.pushButton_CubeW, QtCore.SIGNAL("clicked()"), lambda: Thread_CubeW.START())
 QtCore.QObject.connect(UI.pushButton_OpenGL_CubeW, QtCore.SIGNAL("clicked()"), lambda: Thread_OpenGL_CubeW.START())
 QtCore.QObject.connect(UI.pushButton_Unity_Cube_Stereoscopic, QtCore.SIGNAL("clicked()"), lambda: Thread_Unity_Cube_Stereoscopic.START())
-
+QtCore.QObject.connect(UI.pushButton_Oculus_Cubemap_Generator, QtCore.SIGNAL("clicked()"), lambda: Thread_Oculus_Cubemap_Generator.START())
 
 def set_image_path():
 
